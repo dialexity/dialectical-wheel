c39793b15a55430d4c3b03d609a7940e0d71f4b6
Justin Tan <justin.m.tan@student.ateneo.edu>
2025-06-24 16:13:50 +0800

    updated build

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -5,5 +5,5 @@ export * from '../services';
 export * from '../utils';
 export * from '../store/hooks';
 export * from '../store/dialecticalSlice';
-export * from '../store/store';
+export { store, type RootState, type AppDispatch } from '../store/store';
 //# sourceMappingURL=index.d.ts.map
