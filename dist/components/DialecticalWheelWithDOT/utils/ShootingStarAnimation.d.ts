ff19804f81e3ccda743e0f716b60a470dc692985
Justin Tan <3123justin@gmail.com>
2025-07-11 16:59:21 +0800

    cleaned up old files in /dist

--- dist/components/DialecticalWheelWithDOT/utils/ShootingStarAnimation.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Create shooting star animation with arrowhead
- */
-export declare const createShootingStarAnimation: (arrow: SVGPathElement, onComplete?: () => void) => void;
-//# sourceMappingURL=ShootingStarAnimation.d.ts.map
