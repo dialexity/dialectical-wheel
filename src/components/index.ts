d825d65f9465fa94526e4116c2a1e6dfe549f655
Justin Tan <3123justin@gmail.com>
2025-07-11 16:07:01 +0800

    ExploreCompoenent removed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 // Main exports for dialectical wheel components
 export { default as DialecticalWheel } from './DialecticalWheel';
-export { default as ExploreComponent } from './ExploreComponent';
 export { default as ArrowControls } from './DialecticalWheel/components/ArrowControls';
 export { default as StepControls } from './DialecticalWheel/components/StepControls';
 
